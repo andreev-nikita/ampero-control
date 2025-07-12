85b46c9a769a1ad212082e795f90313450a0331a
Nikita Andreev <nikita.andreev@my.games>
2025-07-13 00:22:16 +0400

    config buttons

--- ampero_control.py
+++ ampero_control.py
@@ -2,25 +2,26 @@ from ableton.v2.control_surface import ControlSurface
 from ableton.v2.control_surface.input_control_element import InputControlElement, MIDI_CC_TYPE
 from .track_fire_button import TrackFireButton
 from .navigation_button import NavigationButton
+from .arm_button import ArmButton
+from .fire_button import FireButton
 
 BUTTONS_CONFIG = [
-    { 'cc_note': 61, 'track': 0 },
-    { 'cc_note': 62, 'track': 1 },
-    { 'cc_note': 63, 'track': 2 },
-    { 'cc_note': 64, 'track': 3 },
-    { 'cc_note': 65, 'track': 4 },
-    { 'cc_note': 66, 'track': 5 },
-    { 'cc_note': 67, 'track': 6 },
-    { 'cc_note': 68, 'track': 7 },
+    { 'cc_note': 20, 'track': 0 },
+    { 'cc_note': 21, 'track': 1 },
+    { 'cc_note': 22, 'track': 2 },
+    { 'cc_note': 23, 'track': 3 },
+    { 'cc_note': 24, 'track': 4 },
+    { 'cc_note': 25, 'track': 5 },
+    { 'cc_note': 26, 'track': 6 },
+    { 'cc_note': 27, 'track': 7 },
 ]
 
 class AmperoControl(ControlSurface):
     def __init__(self, c_instance):
         super().__init__(c_instance=c_instance)
         self.show_message("'AmperoControl' Control Surface has been loaded.")
-        # self._track_fire_buttons = [TrackFireButton(self, button_config['cc_note'], button_config['track']) for button_config in BUTTONS_CONFIG]
-        self.navigate_left_button = NavigationButton(self, 61, 'left')
-        self.navigate_right_button = NavigationButton(self, 62, 'right')
-        self.navigate_top_button = NavigationButton(self, 63, 'up')
-        self.navigate_down_button = NavigationButton(self, 64, 'down')
-        
+        self._navigate_left_button = NavigationButton(self, 16, 'left')
+        self._navigate_right_button = NavigationButton(self, 17, 'right')
+        self._arm_button = ArmButton(self, 18)
+        self._fire_button = FireButton(self, 19)
+        self._track_fire_buttons = [TrackFireButton(self, button_config['cc_note'], button_config['track']) for button_config in BUTTONS_CONFIG] 
--- arm_button.py
+++ arm_button.py
@@ -7,5 +7,8 @@ class ArmButton(Button):
 
     def _on_button_press(self):
         selected_track = self._song.view.selected_track
+        for track in self._song.tracks:
+            if (track.can_be_armed and track != selected_track):
+                track.arm = False
         if (selected_track.can_be_armed):
             selected_track.arm = not selected_track.arm
